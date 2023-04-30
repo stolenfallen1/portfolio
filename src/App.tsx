1354140dcc4f35c36861d178a03a2aef3e258e8b
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-04-30 19:03:19 +0800

    added loader using react-spinners

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 import Hero from "./Views/Hero";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PuffLoader } from "react-spinners";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
-    <BrowserRouter>
-      <div className="m-0 p-0 box-border">
-        <Routes>
-          <Route path="/" element={<Hero />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      {loading ? (
+        <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black">
+          <PuffLoader color="#fff" size={250} />
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Hero />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </div>
   );
 }
 
