667c7e5436663ccbe0127a0f3047ea24df047116
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-31 01:09:21 +0800

    populate projects data

--- src/Components/ProjectLists.tsx
+++ src/Components/ProjectLists.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface ProjectListsProps {
   title: string;
   img: any;
--- src/Views/Projects.tsx
+++ src/Views/Projects.tsx
@@ -1,19 +1,48 @@
-import React from "react";
 import ProjectLists from "../Components/ProjectLists";
+import Eventr from "../Assets/Web/Eventr.png";
+import DIGIBRI from "../Assets/Web/DIGIBRI.png";
+import ToDoApp from "../Assets/Mobile/EXPO_TODO.png";
+import WeatherApp from "../Assets/Mobile/EXPO_WEATHER.png";
 
 const Projects = () => {
-  const title = "HELLO WORLD";
-  const img = "ANY";
-  const content = "HELLO CEBU";
+  const projectDataArray = [
+    {
+      title: "Eventr",
+      img: Eventr,
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptates ex provident repudiandae. Vel quas eligendi aut inventore veniam labore.",
+    },
+    {
+      title: "DIGIBRI",
+      img: DIGIBRI,
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptates ex provident repudiandae. Vel quas eligendi aut inventore veniam labore.",
+    },
+    {
+      title: "EXPO ToDo App",
+      img: ToDoApp,
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptates ex provident repudiandae. Vel quas eligendi aut inventore veniam labore.",
+    },
+    {
+      title: "EXPO Weather App",
+      img: WeatherApp,
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptates ex provident repudiandae. Vel quas eligendi aut inventore veniam labore.",
+    },
+  ];
 
   return (
     <>
-      <div>
-        <ProjectLists title={title} img={img} content={content} />
-      </div>
-      <div>
-        <ProjectLists title={title} img={img} content={content} />
-      </div>
+      {projectDataArray.map((project, index) => (
+        <div key={index}>
+          <ProjectLists
+            title={project.title}
+            img={project.img}
+            content={project.content}
+          />
+        </div>
+      ))}
     </>
   );
 };
