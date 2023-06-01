7692a08751125ab776fb661d9faf0a5edf4c9d6f
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-02 07:02:21 +0800

    minor changes

--- src/Components/ProjectLists.tsx
+++ src/Components/ProjectLists.tsx
@@ -7,18 +7,19 @@ interface ProjectListsProps {
 
 const ProjectLists = ({ title, img, content, index }: ProjectListsProps) => {
   const flexDirectionClass = index % 2 === 0 ? "flex-row" : "flex-row-reverse";
+  const paddingX = index % 2 === 0 ? "pr-40" : "pl-40";
 
   return (
-    <div className="box-border p-0 m-0">
-      <h1 className="pt-20 pb-10 text-white text-4xl font-bold tracking-tighter text-center">
+    <div className="box-border p-0 m-0 h-screen flex flex-col justify-center">
+      <h1 className="pb-10 text-white text-4xl font-bold tracking-tighter text-center">
         {title}
       </h1>
       <div className="flex">
         <div
-          className={`flex ${flexDirectionClass} justify-center items-center px-10`}
+          className={`flex ${flexDirectionClass} justify-center items-center`}
         >
           <img src={img} alt={title} />
-          <p className="text-white">{content}</p>
+          <p className={`text-white ${paddingX} text-justify`}>{content}</p>
         </div>
       </div>
     </div>
