4b112e113df412f4698c9cac3df3b725617c4291
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-02 14:38:11 +0800

    scroll effect project page

--- src/Components/ProjectLists.tsx
+++ src/Components/ProjectLists.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef } from "react";
+
 interface ProjectListsProps {
   title: string;
   img: any;
@@ -6,6 +8,32 @@ interface ProjectListsProps {
 }
 
 const ProjectLists = ({ title, img, content, index }: ProjectListsProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const componentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.2, // Adjust the threshold value as needed
+      }
+    );
+
+    if (componentRef.current) {
+      observer.observe(componentRef.current);
+    }
+
+    return () => {
+      if (componentRef.current) {
+        observer.unobserve(componentRef.current);
+      }
+    };
+  }, []);
+
   const flexDirectionClass =
     index % 2 === 0
       ? "xl:flex-row lg:flex-row"
@@ -13,7 +41,16 @@ const ProjectLists = ({ title, img, content, index }: ProjectListsProps) => {
   const paddingX = index % 2 === 0 ? "xl:pr-40 text-xl" : "xl:pl-40 text-xl";
 
   return (
-    <div className="box-border p-0 m-0 h-screen flex flex-col justify-center">
+    <div
+      className={`box-border p-0 m-0 h-screen flex flex-col justify-center ${
+        isVisible ? "animate-scroll" : ""
+      }`}
+      ref={componentRef}
+      style={{
+        opacity: isVisible ? 1 : 0,
+        transition: "opacity 1.25s ease-in-out",
+      }}
+    >
       <h1 className="pb-10 text-white text-4xl font-bold tracking-tighter text-center">
         {title}
       </h1>
