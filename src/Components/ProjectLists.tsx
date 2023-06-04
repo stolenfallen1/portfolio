3b13cd90e120e8083a5e851b1be8a18ebbeea383
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-05 00:29:06 +0800

    add link repo button

--- src/Components/ProjectLists.tsx
+++ src/Components/ProjectLists.tsx
@@ -59,11 +59,16 @@ const ProjectLists = ({ title, img, content, index }: ProjectListsProps) => {
           className={`flex ${flexDirectionClass} justify-center items-center lg:flex-col md:flex-col sm:flex-col`}
         >
           <img src={img} alt={title} />
-          <p
-            className={`text-white ${paddingX} lg:px-10 md:px-10 sm:px-5 sm:text-base text-justify`}
-          >
-            {content}
-          </p>
+          <div className="">
+            <p
+              className={`text-white ${paddingX} lg:px-10 md:px-10 sm:px-5 sm:text-base text-justify`}
+            >
+              {content}
+            </p>
+            <button className="text-white text-lg border-white border-2 px-3 py-2 mt-4 rounded">
+              Go to Repo
+            </button>
+          </div>
         </div>
       </div>
     </div>
