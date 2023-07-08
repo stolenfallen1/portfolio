304517614228cadaec56e48fe635d31a19fd11d6
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-07-09 00:39:59 +0800

    final skill assets and grid layout not responsive yet

--- src/Components/SkillsContent.tsx
+++ src/Components/SkillsContent.tsx
@@ -3,35 +3,46 @@ import HTML from "../Assets/SkillsLogo/html.png";
 import CSS from "../Assets/SkillsLogo/css.png";
 import JS from "../Assets/SkillsLogo/javascript.png";
 import TS from "../Assets/SkillsLogo/typescript.png";
+import PHP from "../Assets/SkillsLogo/php.png";
 import TAILWIND from "../Assets/SkillsLogo/tailwind.png";
 import BOOTSTRAP from "../Assets/SkillsLogo/bootstrap.png";
 import REACT from "../Assets/SkillsLogo/react.png";
+import EXPO from "../Assets/SkillsLogo/expo.png";
 import NODE from "../Assets/SkillsLogo/nodeJS.png";
 import EXPRESS from "../Assets/SkillsLogo/express.png";
 import MONGO from "../Assets/SkillsLogo/mongoDB.png";
 import FIREBASE from "../Assets/SkillsLogo/firebase.webp";
 import MYSQL from "../Assets/SkillsLogo/mysql.png";
-import PHP from "../Assets/SkillsLogo/php.png";
+import POSTMAN from "../Assets/SkillsLogo/postman.svg";
+import VITE from "../Assets/SkillsLogo/vite.png";
+import GIT from "../Assets/SkillsLogo/git.png";
 
 const SkillsContent = () => {
   return (
-    <div>
-      <ul>
-        <img src={FIGMA} alt="HTML" />
-        <img src={HTML} alt="HTML" />
-        <img src={CSS} alt="HTML" />
-        <img src={JS} alt="HTML" />
-        <img src={TS} alt="HTML" />
-        <img src={PHP} alt="HTML" />
-        <img src={TAILWIND} alt="HTML" />
-        <img src={BOOTSTRAP} alt="HTML" />
-        <img src={REACT} alt="HTML" />
-        <img src={NODE} alt="HTML" />
-        <img src={EXPRESS} alt="HTML" />
-        <img src={MONGO} alt="HTML" />
-        <img src={FIREBASE} alt="HTML" />
-        <img src={MYSQL} alt="HTML" />
-      </ul>
+    <div className="mt-20 px-20 pb-10">
+      <h1 className="text-white text-4xl font-bold tracking-tighter text-center">
+        SKILLS
+      </h1>
+      <div className="grid grid-cols-9 lg:gap-10 items-center mt-16">
+        <img src={FIGMA} alt="HTML" style={{ width: "60px" }} />
+        <img src={HTML} alt="HTML" style={{ width: "85px" }} />
+        <img src={CSS} alt="HTML" style={{ width: "76px" }} />
+        <img src={JS} alt="HTML" style={{ width: "100px" }} />
+        <img src={TS} alt="HTML" style={{ width: "75px" }} />
+        <img src={PHP} alt="HTML" style={{ width: "110px" }} />
+        <img src={TAILWIND} alt="HTML" style={{ width: "110px" }} />
+        <img src={BOOTSTRAP} alt="HTML" style={{ width: "100px" }} />
+        <img src={REACT} alt="HTML" style={{ width: "85px" }} />
+        <img src={EXPO} alt="HTML" style={{ width: "95px" }} />
+        <img src={NODE} alt="HTML" style={{ width: "110px" }} />
+        <img src={EXPRESS} alt="HTML" style={{ width: "110px" }} />
+        <img src={MONGO} alt="HTML" style={{ width: "130px" }} />
+        <img src={FIREBASE} alt="HTML" style={{ width: "130px" }} />
+        <img src={MYSQL} alt="HTML" style={{ width: "130px" }} />
+        <img src={POSTMAN} alt="HTML" style={{ width: "90px" }} />
+        <img src={VITE} alt="HTML" style={{ width: "75px" }} />
+        <img src={GIT} alt="HTML" style={{ width: "75px" }} />
+      </div>
     </div>
   );
 };
